6a5df75fc122ec2aacc018aa8ee2f510d8ccc275
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-03-09 15:38:16 -0300

    new js 1

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-const img = document.getElementById( 'img' );
-const buttons = document.getElementById( 'buttons' );
-let colorIndex = 0;
-let intervalId = null;
+const on = document.getElementById ('on');
+const off = document.getElementById ('off');
+const lamp1 = document.getElementById ('lamp1');
+const lamp2 = document.getElementById ('lamp2');
+const lamp3 = document.getElementById ('lamp3');
+const txt = document.getElementById ( 'txt' );
 
-const trafficLight = ( event ) => {
-    stopAutomatic();
-    turnOn[event.target.id]();
+
+function lampOn () {
+    lamp1.src = 'img/lam-on.png';
+    lamp2.src = "img/lam-on.png";
+    lamp3.src = "img/lam-on.png";
+
+    txt.style.color = '#FFF';
 }
 
-const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+function lampOff () {
+    lamp1.src = "img/lam-off.png";
+    lamp2.src = "img/lam-off.png";
+    lamp3.src = "img/lam-off.png";
 
-const changeColor = () => {
-    const colors = ['red','yellow','green']
-    const color = colors[ colorIndex ];
-    turnOn[color]();
-    nextIndex();
+    txt.style.color = '#000';
 }
 
-const stopAutomatic = () => {
-    clearInterval ( intervalId );
-}
+on.addEventListener ( 'click', lampOn);
 
-const turnOn = {
-    'off':      () => img.src = './img/desligado1.png',
-    'red':      () => img.src = './img/vermelho1.png',
-    'yellow':   () => img.src = './img/amarelo1.png',
-    'green':    () => img.src = './img/verde1.png',
-    'automatic': () => intervalId = setInterval( changeColor, 1000 )
-}
-
-buttons.addEventListener('click', trafficLight );
-
+off.addEventListener ( 'click', lampOff);
